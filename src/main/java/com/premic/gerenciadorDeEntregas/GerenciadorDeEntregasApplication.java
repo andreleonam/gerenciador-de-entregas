1b5c8b89e0acdf159fff552ff0b6e1c1eff286d3
Andre Leonam <andreleonam@hotmail.com>
2023-01-04 11:01:44 -0300

    first commit

---
+++ src/main/java/com/premic/gerenciadorDeEntregas/GerenciadorDeEntregasApplication.java
@@ -0,0 +1,13 @@
+package com.premic.gerenciadorDeEntregas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeEntregasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeEntregasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/premic/gerenciadorDeEntregas/GerenciadorDeEntregasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.premic.gerenciadorDeEntregas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeEntregasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
